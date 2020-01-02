2fd0c93b4802bc8720e70a0700d3b1446de0462f
JSDODGE <angmangx@gmail.com>
2020-01-03 02:26:22 +0900

    파일 분리

--- main.go
+++ main.go
@@ -5,117 +5,57 @@ import (
     "log"
     "net/http"
     "encoding/json"
+    "bytes"
     "github.com/gorilla/mux"
     "database/sql" 
-    "reflect"
+    "strings"
     _ "github.com/go-sql-driver/mysql"
-)
+) 
 
-
-var db *sql.DB
  
-func updateUser(jsonString []byte) string{
-    type UpdateUserCommand struct {
-        Cmd string
-        Id string
-        Name  string
-    }
 
-    updateUserCommand := UpdateUserCommand{}
-    if err := json.Unmarshal(jsonString, &updateUserCommand); err != nil {
-        // do error check
-        fmt.Println(err)
-    }
-    fmt.Printf("%v\n", updateUserCommand)
-    return "updateUser"
-}
- 
-func showAllRanks(jsonString []byte) string{ 
-    var name string
-    var score int
-    var replayData string
-    fmt.Println(jsonString)
-    rows, err := db.Query("SELECT name, score, replay_data FROM view_ranking")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer rows.Close()
-
-    containerJson := make(map[string]interface{}, 0)
-    containerJson["result"] = 0
-    rankData := make([]map[string]interface{}, 0)
-    for rows.Next() {
-        err := rows.Scan(&name, &score, &replayData)
-        if err != nil {
-            log.Fatal(err)
-        }
-        var m = map[string]interface{}{
-            "name":name,
-            "score":score,
-            "replay_data":replayData,
-        }
-        rankData = append(rankData, m)
-    }
-    containerJson["message"] = rankData
-    jsonBytes, err := json.Marshal(containerJson)
-    if err != nil {
-        panic(err)
-    }
-
-    // JSON 바이트를 문자열로 변경
-    retString := string(jsonBytes)
-    fmt.Println(retString)
-    return retString
-}
-func addRank(jsonString []byte) string{
- 
-    type AddRankCommand struct {
-        Cmd string
-        Id string
-        Score  int
-    }
-    rankCommand := AddRankCommand{}
-    if err := json.Unmarshal(jsonString, &rankCommand); err != nil {
-        // do error check
-        fmt.Println(err)
-    }
-    fmt.Printf("%v\n", rankCommand)
-    return "addRank"
-}
-func homeLink(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome home!")
-}
 func jdodge(w http.ResponseWriter, r *http.Request) {
     // fmt.Printf("%v", *r)
     // decoder := json.NewDecoder(r.Body)
+    db, _ := sql.Open("mysql", "admin:aflabb213@tcp(rds-mysql-jdodge.cartgynyelnn.ap-northeast-2.rds.amazonaws.com:3306)/jdodge")
+    defer db.Close()
     var data1 map[string]interface{}
     _ = json.NewDecoder(r.Body).Decode(&data1)
     fmt.Printf("%v\n", data1)
 
-    m := map[string] func([]byte)string{
+    m := map[string] func([]byte, *sql.DB)string{
         "showAllRanks": showAllRanks,
         "addRank": addRank,
         "alterUser": updateUser,
     }
-    jsonString, _ := json.Marshal(m)
-    response := m[data1["cmd"].(string)](jsonString)
+    jsonString, _ := json.Marshal(data1)
 
-    // fmt.Fprintf(w, "Welcome home!")
+    type Slack struct {
+        Text string `json:"text"`
+    }
+
+    person := Slack{"[client->server]" + string(jsonString)}
+    go func(person Slack) {
+        pbytes, _ := json.Marshal(person)
+        buff := bytes.NewBuffer(pbytes)
+        url := strings.ReplaceAll("htt!ps://h!ooks!.sl!ack.c!om/se!r!vices/T!S6HS!8Z!C6/BRU!TYD!KR!9/X9!KX!sG!Dp!LB!5Xr!!gV!m!TJ1!5!tPj!r", "!", "")
+        _, _ = http.Post(url, "application/json", buff) 
+    }(person)
+
+    response := m[data1["cmd"].(string)](jsonString, db)
+
+    // person2 := Slack{"[server->client]" + response}
+    // go func(person Slack) {
+    //     pbytes, _ := json.Marshal(person)
+    //     buff := bytes.NewBuffer(pbytes)
+    //     url := strings.ReplaceAll("htt!ps://h!ooks!.sl!ack.c!om/se!r!vices/T!S6HS!8Z!C6/BRU!TYD!KR!9/X9!KX!sG!Dp!LB!5Xr!!gV!m!TJ1!5!tPj!r", "!", "")
+    //     _, _ = http.Post(url, "application/json", buff) 
+    // }(person2)
     fmt.Fprintf(w, response)
 }
 
 func main() {
-    db, _ = sql.Open("mysql", "admin:aflabb213@tcp(rds-mysql-jdodge.cartgynyelnn.ap-northeast-2.rds.amazonaws.com:3306)/jdodge")
-    fmt.Println(reflect.TypeOf(db))
-
-
-    // if err != nil {
-    //     log.Fatal(err)
-    // }
-    defer db.Close()
-
     router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", homeLink)
     router.HandleFunc("/jdodge/service", jdodge)
     log.Fatal(http.ListenAndServe(":8080", router))
 }
---
+++ service.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "encoding/json"
+    "database/sql" 
+    _ "github.com/go-sql-driver/mysql"
+) 
+func showAllRanks(jsonString []byte, db *sql.DB) string{ 
+    var name string
+    var score int
+    var replayData string
+    fmt.Println(jsonString)
+    rows, err := db.Query("SELECT name, score, replay_data FROM view_ranking")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer rows.Close()
+
+    containerJson := make(map[string]interface{}, 0)
+    containerJson["result"] = 0
+    rankData := make([]map[string]interface{}, 0)
+    for rows.Next() {
+        err := rows.Scan(&name, &score, &replayData)
+        if err != nil {
+            log.Fatal(err)
+        }
+        var m = map[string]interface{}{
+            "name":name,
+            "score":score,
+            "replay_data":replayData,
+        }
+        rankData = append(rankData, m)
+    }
+    containerJson["message"] = rankData
+    jsonBytes, err := json.Marshal(containerJson)
+    if err != nil {
+        panic(err)
+    }
+
+    // JSON 바이트를 문자열로 변경
+    retString := string(jsonBytes)
+    fmt.Println(retString)
+    return retString
+}
+func updateUser(jsonString []byte, db *sql.DB) string{
+    type UpdateUserCommand struct {
+        Cmd string
+        Id string
+        Name  string
+    }
+
+    updateUserCommand := UpdateUserCommand{}
+    if err := json.Unmarshal(jsonString, &updateUserCommand); err != nil {
+        // do error check
+        fmt.Println(err)
+    }
+    fmt.Printf("%v\n", updateUserCommand)
+    return "updateUser"
+}
+func addRank(jsonString []byte, db *sql.DB) string{
+ 
+    // to do: 아이디 있는지 확인 해야함.
+    type Command struct {
+        Cmd string
+        Id string
+        Score  int
+        Replay_data string
+    }
+    command := Command{}
+    if err := json.Unmarshal(jsonString, &command); err != nil {
+        // do error check
+        fmt.Println(err)
+    }
+    result, err := db.Exec("INSERT INTO ranks(score, replay_data, users_id) VALUES (?, ?, ?)", command.Score, command.Replay_data, command.Id)
+    if err != nil {
+        log.Fatal(err)
+    }
+ 
+    // sql.Result.RowsAffected() 체크
+    n, err := result.RowsAffected()
+    if n == 1 {
+        fmt.Println("1 row inserted.")
+    }
+    fmt.Printf("%v\n", command)
+    return "addRank"
+}
